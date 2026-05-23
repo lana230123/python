6d725ac9ce7c6e17154cb8e6c1e3eff20904f7d6
lana230123 <laura.n230123@gmail.com>
2026-05-23 16:21:21 +0700

    Add files via upload

---
+++ Basic Exercise.py
@@ -0,0 +1,12 @@
+name = input(str("Name: "))
+origin = input(str("Origin:"))
+profession = input(str("Profession: "))
+country = input(str("Country of origin:"))
+TYL = input(str("Things you like: "))
+
+print(name)
+print(origin)
+print(profession)
+print(country)
+print(TYL)
+
